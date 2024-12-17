1aa7a2a71f1950fbb9e0469d214a2a64d34e4af3
hearsay316 <hearsay316@gmail.com>
2024-12-17 23:51:28 +0800

    feat: 用rust随机生成n个2维坐标的集合s，再提供一个方法，输入一个坐标p，从s里返回离p最近的m个坐标

---
+++ src/coord/mod.rs
@@ -0,0 +1,103 @@
+use rand::Rng;
+
+#[derive(Debug, Clone)]
+pub struct Point {
+    x: PointVal,
+    y: PointVal,
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64) -> Self {
+        Self {
+            x: PointVal::from_f64(x),
+            y: PointVal::from_f64(y),
+        }
+    }
+    fn distance(&self, other: &Point) -> PointVal {
+        let dx = self.x.to_f64() - other.x.to_f64();
+        let dy = self.y.to_f64() - other.y.to_f64();
+        PointVal::from_f64((dx * dx + dy * dy).sqrt())
+    }
+}
+#[derive(Debug, Clone)]
+struct PointVal {
+    integral: String,
+    fractional: String,
+}
+
+impl PointVal {
+    fn new(i: u64, f: u64) -> Self {
+        Self {
+            integral: i.to_string(),
+            fractional: f.to_string(),
+        }
+    }
+    fn from_string(i: String, f: String) -> Self {
+        Self {
+            integral: i,
+            fractional: f,
+        }
+    }
+    fn rng_new() -> PointVal {
+        let mut rng = rand::thread_rng();
+        let i = rng.gen_range(0..100);
+        let f = rng.gen_range(0..100);
+        PointVal::new(i, f)
+    }
+    fn from_f64(f: f64) -> PointVal {
+        let binding = f.to_string();
+        let mut vec = binding.split('.').collect::<Vec<_>>();
+        //补零
+        if vec.len() == 1 {
+            vec.push("0");
+        }
+        PointVal::from_string(vec[0].into(), vec[1].into())
+    }
+    fn to_f64(&self) -> f64 {
+        format!("{}.{}", self.integral, self.fractional)
+            .parse::<f64>()
+            .unwrap()
+    }
+
+    fn to_u64(&self, len: i64) -> u64 {
+        let i = self.integral.parse::<u64>().unwrap();
+        let num = (0..len)
+            .fold(1, |acc, _| acc * 10);
+        i * num + self.fractional.parse::<u64>().unwrap()
+    }
+}
+
+#[derive(Debug)]
+pub struct Coordinate {
+    coord: Vec<Point>,
+}
+
+impl Coordinate {
+    pub fn new(n: usize) -> Self {
+        Self {
+            coord: (0..n)
+                .map(|_| Point {
+                    x: PointVal::rng_new(),
+                    y: PointVal::rng_new(),
+                })
+                .collect(),
+        }
+    }
+
+    pub fn find_closest(&self, target: &Point, m: usize) -> Vec<Point> {
+        let mut vec = Vec::new();
+        let mut length = 1;
+        for point in &self.coord {
+            let dist = point.distance(&target);
+            if dist.fractional.len() > length {
+                length = dist.fractional.len();
+            }
+            vec.push((dist, point.clone()));
+        }
+        //排序小到大
+        vec.sort_by(|a, b| a.0.to_u64(length as i64).cmp(&b.0.to_u64(length as i64)));
+        // 取出来m个数据
+        vec.iter().map(|(_, point)|point.clone()).take(m).collect::<Vec<Point>>()
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use coordinate::coord::{Coordinate, Point};
+
+// coordinate
+fn main() {
+    let points = Coordinate::new(10);
+    let target = Point::new(50.0, 50.0); // 目标坐标，可以按需修改
+    let m = 3; // 要获取的最近坐标数量，可以按需修改
+    let closest_points = points.find_closest(&target, m);
+    println!("随机生成的坐标集合: {:?}", points);
+    println!(
+        "距离目标坐标 最近的 {} 个坐标: {:?}",
+         m, closest_points
+    );
+}
+
