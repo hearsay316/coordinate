bcb35104b7c838b517cdce8b582444b8460812c6
hearsay316 <hearsay316@gmail.com>
2024-12-19 20:13:32 +0800

    refactor(coord): 重构坐标模块并添加新版本
    - 新增 coord_v2.rs 文件，实现了一个更简洁的坐标系统
    -保留原有的 coord.rs 文件，重命名为 coord_v2.rs
    - 更新 main.rs 和 mod.rs 文件，引入新的 Coordinate 和 Point 结构
    -重构 Point 和 Coordinate 的实现，简化了距离计算和最近点查找逻辑

---
+++ src/coord/coord_v2.rs
@@ -0,0 +1,56 @@
+
+use rand::Rng;
+
+#[derive(Debug, Clone)]
+pub struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+    fn distance(&self, other: &Point) -> f64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+       (dx * dx + dy * dy).sqrt()
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Coordinate {
+    coord: Vec<Point>,
+}
+
+impl Coordinate {
+    pub fn new(n: usize) -> Self {
+        let mut rng = rand::thread_rng();
+        Self {
+            coord: (0..n)
+                .map(|_| Point {
+                    x: rng.gen_range(0.0..100.0),
+                    y: rng.gen_range(0.0..100.0),
+                })
+                .collect(),
+        }
+    }
+
+    pub fn find_closest(&self, target: &Point, m: usize) -> Vec<Point> {
+        let mut vec = Vec::new();
+        for point in &self.coord {
+            let dist = point.distance(&target);
+            vec.push((dist, point.clone()));
+        }
+        //排序小到大
+        vec.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(std::cmp::Ordering::Equal));
+        println!("{:?}",vec);
+        // 取出来m个数据
+        vec.iter().map(|(_, point)|point.clone()).take(m).collect::<Vec<Point>>()
+    }
+}
+
