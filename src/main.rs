e0bdef0c4aac10adaf72967478e83793fda8c929
hearsay316 <hearsay316@gmail.com>
2024-12-23 22:28:21 +0800

    refactor(src/main.rs): 添加代码注释以增强可读性- 为初始化数量和目标坐标添加注释 - 解释了要获取的最近坐标数量的用途

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use coordinate::coord::coord_v2::{Coordinate, Point};
 
 // coordinate
 fn main() {
-    let points = Coordinate::new(10);
+    let points = Coordinate::new(10); //初始化数量
     let target = Point::new(50.0, 50.0); // 目标坐标，可以按需修改
     let m = 3; // 要获取的最近坐标数量，可以按需修改
     let closest_points = points.find_closest(&target, m);
