34b8f28ff70267e4fa368807f6e6cfb4ffa74359
hearsay316 <hearsay316@gmail.com>
2025-01-04 02:21:49 +0800

    feat:优化 API 并添加文件上传功能      创建 vue-up-data 项目
    - 初始化 Vue 3 项目结构
    - 添加基本的组件、路由和状态管理
    - 配置 ESLint、Prettier等开发工具
    - 创建基础样式文件
    - 添加图标组件- 实现简单的文件上传功能
    - 更新 Cargo.toml，添加 axum 和 tower-http 的特定版本和功能
    -重构 path_hashmap 示例，增加对 URI、Headers 和 Query 的处理- 添加文件上传处理功能，支持文本和二进制数据
    - 集成 CORS 支持，配置允许的源和方法

--- examples/path_hashmap.rs
+++ examples/path_hashmap.rs
@@ -1,24 +1,64 @@
-use axum::{
-    extract::Path,
-    routing::get,
-    Router,
-};
+use axum::extract::{Multipart, OriginalUri, Query};
+use axum::http::{HeaderMap, HeaderValue, Method, StatusCode, Uri};
+use axum::{extract::Path, routing::get, Router};
 use std::collections::HashMap;
+use axum::response::{IntoResponse, Response};
 use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
 
-async fn users_get(Path(params): Path<HashMap<String, String>>)->String {
+async fn users_get(
+    uri: Uri,
+    OriginalUri(original_uri): OriginalUri,
+    Path(params): Path<HashMap<String, String>>,
+    headers: HeaderMap,
+    Query(query): Query<HashMap<String, String>>
+) -> String {
     // Both `version` and `id` were captured even though `users_api` only
     // explicitly captures `id`.
-    let version = params.get("version");
-    let id = params.get("id");
-    println!("{:?}",params);
+    println!("{:?}", uri);
+    println!("{:?}", original_uri);
+    println!("{:?}", params);
+    println!("{:?}",headers);
+    println!("{:?}",query);
     "这个是一个请求".to_string()
 }
-#[tokio::main]
-async fn main(){
-    let users_api = Router::new().route("/users/{id}", get(users_get));
+async fn accept_form(mut multipart: Multipart)-> Response  {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let file_name = field.file_name().unwrap_or("").to_string();
+        let content_type = field.content_type().unwrap_or("").to_string();
+        if name == "text" {
+            // 保存文件
+            let text = field.text().await.unwrap();
+            println!(
+                "Length of  (`{name}`: `{text}`) ",
+            );
+        }else {
+            let data = field.bytes().await.unwrap();
+            println!(
+                "Length of `{name}` (`{file_name}`: `{content_type}`) is {} bytes",
+                data.len()
+            );
+        }
+    };
+    (StatusCode::OK,  "上传成功".to_string()).into_response()
 
-    let app = Router::new().nest("/{version}/api", users_api);
+}
+#[tokio::main]
+async fn main() {
+    let users_api = Router::new().route("/users/{id}", get(users_get).post(accept_form));
+
+    let app = Router::new().nest("/{version}/api", users_api).layer(
+        // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
+        // for more details
+        //
+        // pay attention that for some request types like posting content-type: application/json
+        // it is required to add ".allow_headers([http::header::CONTENT_TYPE])"
+        // or see this issue https://github.com/tokio-rs/axum/issues/849
+        CorsLayer::new()
+            .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+            .allow_methods([Method::GET,Method::POST]),
+    );
     let tcp = TcpListener::bind("0.0.0.0:3303").await.unwrap();
-    axum::serve(tcp,app).await.unwrap();
+    axum::serve(tcp, app).await.unwrap();
 }
